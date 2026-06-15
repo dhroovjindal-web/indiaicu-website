15feea62721d2e654568ff97c798568fc1211ad9
dhroovjindal-web <dhroovjindal@gmail.com>
2026-06-15 14:44:07 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ const PHONE_NUMBER = "8901434774";
 const PHONE_DISPLAY = "89014 34774";
 const PHONE_TEL = `tel:+91${PHONE_NUMBER}`;
 const WHATSAPP_URL = `https://wa.me/918901434261`;
-const SUPPORT_EMAIL = "dhroovjindal@gmail.com";
+const SUPPORT_EMAIL = "connect.indiaicu@gmail.com";
 const CURRENT_YEAR = new Date().getFullYear();
 
 export function Footer() {
